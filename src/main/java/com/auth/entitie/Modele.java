9b8ddc78f73090541c332fca1d29316442728172
halima <halima@gmail.com>
2019-06-27 23:54:05 +0100

    45 commit

--- src/main/java/com/auth/entitie/Modele.java
+++ src/main/java/com/auth/entitie/Modele.java
@@ -17,15 +17,15 @@ public class Modele {
 	private int id_modele ;
 	private String des_modele ;
 	
-	@OneToOne
-	@JoinColumn(name="id_marque")
-	private Marque marque_m ;
-	
-	@JsonProperty("id_marque")
-	private void unpackmarque(Integer id_marque) {
-	    this.marque_m = new Marque();
-	    marque_m.setId_marque(id_marque);
-	}
+//	@OneToOne
+//	@JoinColumn(name="id_marque")
+//	private Marque marque_m ;
+//	
+//	@JsonProperty("id_marque")
+//	private void unpackmarque(Integer id_marque) {
+//	    this.marque_m = new Marque();
+//	    marque_m.setId_marque(id_marque);
+//	}
 	
 	public int getId_modele() {
 		return id_modele;
@@ -41,17 +41,17 @@ public class Modele {
 	}
 	
 	
-	public Marque getMarque_m() {
-		return marque_m;
-	}
-	public void setMarque_m(Marque marque_m) {
-		this.marque_m = marque_m;
-	}
+//	public Marque getMarque_m() {
+//		return marque_m;
+//	}
+//	public void setMarque_m(Marque marque_m) {
+//		this.marque_m = marque_m;
+//	}
 	public Modele(int id_modele, String des_modele , Marque marque_m) {
 		super();
 		this.id_modele = id_modele;
 		this.des_modele = des_modele;
-		this.marque_m = marque_m ;
+		//this.marque_m = marque_m ;
 	}
 	public Modele() {
 		super();
